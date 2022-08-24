f9048cd0e00dcc5fe145df40c8f24caea40ee655
ytallo <ytallo.layon@kovi.com.br>
2022-08-24 09:51:29 -0300

    chore: added ngw, private subnets and route tables

--- aws-infrastructure/modules/kad-vpc/route-tables.tf
+++ aws-infrastructure/modules/kad-vpc/route-tables.tf
@@ -21,6 +21,12 @@ resource "aws_route_table" "kad-public-route-table" {
   tags = merge({ Name = "kad-public-route-table" }, var.tags)
 }
 
+resource "aws_route_table" "kad-private-route-table" {
+  vpc_id = aws_vpc.kad-vpc.id
+
+  tags = merge({ Name = "kad-private-route-table" }, var.tags)
+}
+
 resource "aws_route_table_association" "kad-public-route-table-public-subnet-a" {
   subnet_id      = aws_subnet.kad-public-subnet-a.id
   route_table_id = aws_route_table.kad-public-route-table.id
@@ -34,4 +40,19 @@ resource "aws_route_table_association" "kad-public-route-table-public-subnet-b" 
 resource "aws_route_table_association" "kad-public-route-table-public-subnet-c" {
   subnet_id      = aws_subnet.kad-public-subnet-c.id
   route_table_id = aws_route_table.kad-public-route-table.id
+}
+
+resource "aws_route_table_association" "kad-public-route-table-private-subnet-a" {
+  subnet_id      = aws_subnet.kad-private-subnet-a.id
+  route_table_id = aws_route_table.kad-private-route-table.id
+}
+
+resource "aws_route_table_association" "kad-private-route-table-private-subnet-b" {
+  subnet_id      = aws_subnet.kad-private-subnet-b.id
+  route_table_id = aws_route_table.kad-private-route-table.id
+}
+
+resource "aws_route_table_association" "kad-private-route-table-private-subnet-c" {
+  subnet_id      = aws_subnet.kad-private-subnet-c.id
+  route_table_id = aws_route_table.kad-private-route-table.id
 }
--- aws-infrastructure/modules/kad-vpc/subnets.tf
+++ aws-infrastructure/modules/kad-vpc/subnets.tf
@@ -26,3 +26,27 @@ resource "aws_subnet" "kad-public-subnet-c" {
 
   tags = merge({ Name = "kad-public-subnet-c" }, var.tags)
 }
+
+resource "aws_subnet" "kad-private-subnet-a" {
+  vpc_id                  = aws_vpc.kad-vpc.id
+  availability_zone       = "${data.aws_region.current_region.name}a"
+  cidr_block              = cidrsubnet(aws_vpc.kad-vpc.cidr_block, 4, 3)
+
+  tags = merge({ Name = "kad-private-subnet-a" }, var.tags)
+}
+
+resource "aws_subnet" "kad-private-subnet-b" {
+  vpc_id                  = aws_vpc.kad-vpc.id
+  availability_zone       = "${data.aws_region.current_region.name}b"
+  cidr_block              = cidrsubnet(aws_vpc.kad-vpc.cidr_block, 4, 4)
+
+  tags = merge({ Name = "kad-private-subnet-b" }, var.tags)
+}
+
+resource "aws_subnet" "kad-private-subnet-c" {
+  vpc_id                  = aws_vpc.kad-vpc.id
+  availability_zone       = "${data.aws_region.current_region.name}c"
+  cidr_block              = cidrsubnet(aws_vpc.kad-vpc.cidr_block, 4, 5)
+
+  tags = merge({ Name = "kad-private-subnet-c" }, var.tags)
+}
